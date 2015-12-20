484d29323d850deb66deb7a254a41945392a9319
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-12-20 19:28:59 +0100

    deliv 51

---
+++ championships/week51/_deliveries.sh
@@ -0,0 +1,399 @@
+#  Started at :2015-12-20 19:21:16
+
+
+####
+## Handling Equipe AA [qaa]
+####
+# Cloning project from remote repository: OK
+# Opening project qaa
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 15:30:54 2015 +0100
+# LOC counter [main]: 1935
+# LOC counter [test]: 606
+# Building using Maven [clean package]: FAILURE
+## Abort [project does not build]
+# Leaving project qaa
+
+####
+## Handling Equipe AB [qab]
+####
+# Cloning project from remote repository: OK
+# Opening project qab
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 15:26:16 2015 +0100
+# LOC counter [main]: 1643
+# LOC counter [test]: 949
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qab]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qab]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qab/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qab
+
+####
+## Handling Equipe AC [qac]
+####
+# Cloning project from remote repository: OK
+# Opening project qac
+# Checking out tag [week51]: FAILURE
+## Abort [missing tag]
+# Leaving project qac
+
+####
+## Handling Equipe AD [qad]
+####
+# Cloning project from remote repository: OK
+# Opening project qad
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 14:08:29 2015 +0100
+# LOC counter [main]: 1703
+# LOC counter [test]: 534
+# Building using Maven [clean package]: FAILURE
+## Abort [project does not build]
+# Leaving project qad
+
+####
+## Handling Equipe AE [qae]
+####
+# Cloning project from remote repository: OK
+# Opening project qae
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 18:46:08 2015 +0100
+# LOC counter [main]: 2271
+# LOC counter [test]: 1139
+# Building using Maven [clean package]: FAILURE
+## Abort [project does not build]
+# Leaving project qae
+
+####
+## Handling Equipe AF [qaf]
+####
+# Cloning project from remote repository: OK
+# Opening project qaf
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 18:31:50 2015 +0100
+# LOC counter [main]: 626
+# LOC counter [test]: 146
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qaf]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qaf]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qaf/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qaf
+
+####
+## Handling Equipe BA [qba]
+####
+# Cloning project from remote repository: OK
+# Opening project qba
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 17:56:24 2015 +0100
+# LOC counter [main]: 1573
+# LOC counter [test]: 848
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qba]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qba]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qba/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qba
+
+####
+## Handling Equipe BB [qbb]
+####
+# Cloning project from remote repository: OK
+# Opening project qbb
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 18:36:05 2015 +0100
+# LOC counter [main]: 1976
+# LOC counter [test]: 1098
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qbb]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbb]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbb/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qbb
+
+####
+## Handling Equipe BC [qbc]
+####
+# Cloning project from remote repository: OK
+# Opening project qbc
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 16:03:27 2015 +0100
+# LOC counter [main]: 1645
+# LOC counter [test]: 757
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qbc]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbc]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbc/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qbc
+
+####
+## Handling Equipe BD [qbd]
+####
+# Cloning project from remote repository: OK
+# Opening project qbd
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Mon Dec 14 09:51:48 2015 +0100
+# LOC counter [main]: 980
+# LOC counter [test]: 242
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qbd]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbd]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbd/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qbd
+
+####
+## Handling Equipe BE [qbe]
+####
+# Cloning project from remote repository: OK
+# Opening project qbe
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 17:12:20 2015 +0100
+# LOC counter [main]: 833
+# LOC counter [test]: 495
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qbe]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbe]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbe/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qbe
+
+####
+## Handling Equipe BF [qbf]
+####
+# Cloning project from remote repository: OK
+# Opening project qbf
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 16:25:12 2015 +0100
+# LOC counter [main]: 1158
+# LOC counter [test]: 127
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qbf]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbf]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qbf/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qbf
+
+####
+## Handling Equipe CA [qca]
+####
+# Cloning project from remote repository: OK
+# Opening project qca
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 19:03:58 2015 +0100
+# LOC counter [main]: 2460
+# LOC counter [test]: 792
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qca]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qca]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qca/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qca
+
+####
+## Handling Equipe CB [qcb]
+####
+# Cloning project from remote repository: OK
+# Opening project qcb
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 18:27:31 2015 +0100
+# LOC counter [main]: 3921
+# LOC counter [test]: 1666
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qcb]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qcb]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qcb/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qcb
+
+####
+## Handling Equipe CC [qcc]
+####
+# Cloning project from remote repository: OK
+# Opening project qcc
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 17:07:48 2015 +0100
+# LOC counter [main]: 1570
+# LOC counter [test]: 829
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qcc]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qcc]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qcc/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qcc
+
+####
+## Handling Equipe CD [qcd]
+####
+# Cloning project from remote repository: OK
+# Opening project qcd
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 15:26:11 2015 +0100
+# LOC counter [main]: 1724
+# LOC counter [test]: 784
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qcd]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qcd]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qcd/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qcd
+
+####
+## Handling Equipe CE [qce]
+####
+# Cloning project from remote repository: OK
+# Opening project qce
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 17:49:48 2015 +0100
+# LOC counter [main]: 2152
+# LOC counter [test]: 768
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qce]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qce]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qce/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qce
+
+####
+## Handling Equipe CF [qcf]
+####
+# Cloning project from remote repository: OK
+# Opening project qcf
+# Checking out tag [week51]: FAILURE
+## Abort [missing tag]
+# Leaving project qcf
+
+####
+## Handling Equipe DA [qda]
+####
+# Cloning project from remote repository: OK
+# Opening project qda
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 16:10:53 2015 +0100
+# LOC counter [main]: 3529
+# LOC counter [test]: 158
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qda]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qda]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qda/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qda
+
+####
+## Handling Equipe DB [qdb]
+####
+# Cloning project from remote repository: OK
+# Opening project qdb
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 18:16:09 2015 +0100
+# LOC counter [main]: 3965
+# LOC counter [test]: 4464
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qdb]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qdb]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qdb/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qdb
+
+####
+## Handling Equipe DC [qdc]
+####
+# Cloning project from remote repository: OK
+# Opening project qdc
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 18:56:57 2015 +0100
+# LOC counter [main]: 1712
+# LOC counter [test]: 1005
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qdc]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qdc]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qdc/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qdc
+
+####
+## Handling Equipe DD [qdd]
+####
+# Cloning project from remote repository: OK
+# Opening project qdd
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 01:45:29 2015 +0100
+# LOC counter [main]: 1466
+# LOC counter [test]: 411
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qdd]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qdd]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qdd/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qdd
+
+####
+## Handling Equipe DE [qde]
+####
+# Cloning project from remote repository: OK
+# Opening project qde
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Sun Dec 20 18:08:52 2015 +0100
+# LOC counter [main]: 1354
+# LOC counter [test]: 361
+# Building using Maven [clean package]: OK
+## \o/ Delivery OK \o/
+# pom.xml/groupId == [fr.unice.polytech.3a.qgl]: OK
+# pom.xml/artifactId == [qde]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qde]: OK
+# Checking existence of [./src/main/java/fr/unice/polytech/qgl/qde/Explorer.java]: OK
+## \o/ Environment OK, will run the championship \o/
+# Leaving project qde
+
+####
+## Handling Equipe DF [qdf]
+####
+# Cloning project from remote repository: OK
+# Opening project qdf
+# Checking out tag [week51]: OK
+# Git stamp: Date:   Thu Dec 17 10:25:46 2015 +0100
+# LOC counter [main]: 1083
+# LOC counter [test]: 570
+# Building using Maven [clean package]: FAILURE
+## Abort [project does not build]
+# Leaving project qdf
+
+#  Ended at :2015-12-20 19:26:57
+
