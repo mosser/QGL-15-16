9c717fc6138551aa8efed56c2fd24f35bf711d3b
Sebastien Mosser <sebastien.mosser@gmail.com>
2016-03-28 18:55:54 +0200

    retrospective final edit

--- arena/src/main/scala/library/SI3.scala
+++ arena/src/main/scala/library/SI3.scala
@@ -5,44 +5,44 @@ import eu.ace_design.island.bot.IExplorerRaid
 
 trait SI3 extends Teams {
 
-  def all: Map[String, IExplorerRaid] = g1 ++ g2 ++ g3 ++ g4
+  def all:  Map[String, Class[_ <: IExplorerRaid]] = g1 ++ g2 ++ g3 ++ g4
 
   def players = all
 
-  private lazy val g1: Map[String, IExplorerRaid] = Map(
-    "qaa" -> new fr.unice.polytech.qgl.qaa.Explorer(),
-    "qab" -> new fr.unice.polytech.qgl.qab.Explorer(),
-    "qac" -> new fr.unice.polytech.qgl.qac.Explorer(),
-    "qad" -> new fr.unice.polytech.qgl.qad.Explorer(),
-    "qae" -> new fr.unice.polytech.qgl.qae.Explorer(),
-    "qaf" -> new fr.unice.polytech.qgl.qaf.Explorer()
+  private lazy val g1: Map[String, Class[_ <: IExplorerRaid]] = Map(
+    "qaa" -> classOf[fr.unice.polytech.qgl.qaa.Explorer],
+    "qab" -> classOf[fr.unice.polytech.qgl.qab.Explorer],
+    "qac" -> classOf[fr.unice.polytech.qgl.qac.Explorer],
+    "qad" -> classOf[fr.unice.polytech.qgl.qad.Explorer],
+    "qae" -> classOf[fr.unice.polytech.qgl.qae.Explorer],
+    "qaf" -> classOf[fr.unice.polytech.qgl.qaf.Explorer]
   )
 
-  private lazy val g2: Map[String, IExplorerRaid] = Map(
-    "qba" -> new fr.unice.polytech.qgl.qba.Explorer(),
-    "qbb" -> new fr.unice.polytech.qgl.qbb.Explorer(),
-    "qbc" -> new fr.unice.polytech.qgl.qbc.Explorer(),
-    "qbd" -> new fr.unice.polytech.qgl.qbd.Explorer(),
-    "qbe" -> new fr.unice.polytech.qgl.qbe.Explorer(),
-    "qbf" -> new fr.unice.polytech.qgl.qbf.Explorer()
+  private lazy val g2:  Map[String, Class[_ <: IExplorerRaid]] = Map(
+    "qba" -> classOf[fr.unice.polytech.qgl.qba.Explorer],
+    "qbb" -> classOf[fr.unice.polytech.qgl.qbb.Explorer],
+    "qbc" -> classOf[fr.unice.polytech.qgl.qbc.Explorer],
+    "qbd" -> classOf[fr.unice.polytech.qgl.qbd.Explorer],
+    "qbe" -> classOf[fr.unice.polytech.qgl.qbe.Explorer],
+    "qbf" -> classOf[fr.unice.polytech.qgl.qbf.Explorer]
   )
 
-  private lazy val g3: Map[String, IExplorerRaid]= Map(
-    "qca" -> new fr.unice.polytech.qgl.qca.Explorer(),
-    "qcb" -> new fr.unice.polytech.qgl.qcb.Explorer(),
-    "qcc" -> new fr.unice.polytech.qgl.qcc.Explorer(),
-    "qcd" -> new fr.unice.polytech.qgl.qcd.Explorer(),
-    "qce" -> new fr.unice.polytech.qgl.qce.Explorer(),
-    "qcf" -> new fr.unice.polytech.qgl.qcf.Explorer()
+  private lazy val g3:  Map[String, Class[_ <: IExplorerRaid]]= Map(
+    "qca" -> classOf[fr.unice.polytech.qgl.qca.Explorer],
+    "qcb" -> classOf[fr.unice.polytech.qgl.qcb.Explorer],
+    "qcc" -> classOf[fr.unice.polytech.qgl.qcc.Explorer],
+    "qcd" -> classOf[fr.unice.polytech.qgl.qcd.Explorer],
+    "qce" -> classOf[fr.unice.polytech.qgl.qce.Explorer],
+    "qcf" -> classOf[fr.unice.polytech.qgl.qcf.Explorer]
   )
 
-  private lazy val g4: Map[String, IExplorerRaid]= Map(
-    "qda" -> new fr.unice.polytech.qgl.qda.Explorer(),
-    "qdb" -> new fr.unice.polytech.qgl.qdb.Explorer(),
-    "qdc" -> new fr.unice.polytech.qgl.qdc.Explorer(),
-    "qdd" -> new fr.unice.polytech.qgl.qdd.Explorer(),
-    "qde" -> new fr.unice.polytech.qgl.qde.Explorer(),
-    "qdf" -> new fr.unice.polytech.qgl.qdf.Explorer()
+  private lazy val g4:  Map[String, Class[_ <: IExplorerRaid]] = Map(
+    "qda" -> classOf[fr.unice.polytech.qgl.qda.Explorer],
+    "qdb" -> classOf[fr.unice.polytech.qgl.qdb.Explorer],
+    "qdc" -> classOf[fr.unice.polytech.qgl.qdc.Explorer],
+    "qdd" -> classOf[fr.unice.polytech.qgl.qdd.Explorer],
+    "qde" -> classOf[fr.unice.polytech.qgl.qde.Explorer],
+    "qdf" -> classOf[fr.unice.polytech.qgl.qdf.Explorer]
   )
 
 }
